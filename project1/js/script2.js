caa8dcc4a5352dfbb32f653c5c8b9631e00d92f6
donerin <donerin1404@gmail.com>
2022-09-14 13:12:40 +0300

    add new changes

--- project1/js/script2.js
+++ project1/js/script2.js
@@ -1,2 +1,16 @@
-const hello = 'Hello, World!'
-console.log(hello);
+const sumSquareDifference = (number) => {
+   let counter = 1;
+   let sqCounter = 0;
+   let squareCounter = 0;
+
+
+   while (counter <= number) {
+      sqCounter += counter ** 2;
+      squareCounter += counter;
+      counter += 1;
+   }
+   return (squareCounter ** 2) - sqCounter;
+}
+
+
+console.log(sumSquareDifference(42));
